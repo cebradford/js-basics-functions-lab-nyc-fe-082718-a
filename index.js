3b8c1a9d7bb652d03d696c2a4488e2aa25fe4025
casey bradford <casey.bradford@wework.com>
2018-10-07 19:22:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(location){
+  return Math.abs(42 - location);
+}
+
+function distanceFromHqInFeet(location){
+  return ((Math.abs(42 - location))*264)
+}
+
+function distanceTravelledInFeet(location, destination){
+  
+  return ((Math.abs(destination - location))*264)
+}
+
+function calculatesFarePrice(start, destination){
+  return (((Math.abs(destination - location))*264) - 400) + 
+}
